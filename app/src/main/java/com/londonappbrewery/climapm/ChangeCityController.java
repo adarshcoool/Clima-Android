257e75c5a6cb107732262764a5c6d69b444c1c32
Adarsh Yadav <adarshyadav20@gmail.com>
2017-09-11 15:12:21 +0530

    Weather Mobile Application Completed!

---
+++ app/src/main/java/com/londonappbrewery/climapm/ChangeCityController.java
@@ -0,0 +1,41 @@
+package com.londonappbrewery.climapm;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class ChangeCityController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.change_city_layout);
+        final EditText editTextField = (EditText) findViewById(R.id.queryET);
+        ImageButton backButton = (ImageButton) findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        editTextField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                String newCity = editTextField.getText().toString();
+                Intent newCityIntent = new Intent(ChangeCityController.this, WeatherController.class);
+                newCityIntent.putExtra("City", newCity);
+                startActivity(newCityIntent);
+
+                return false;
+            }
+        });
+    }
+}
